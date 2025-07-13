39820117b98de0d1f3871cdbe23e0b7d20ce5baf
md-mejbaul-haque <mejba.s@gmail.com>
2025-07-14 01:20:25 +0600

    Create states csv file with missing states

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ while len(guessed_states) < 50:
         for state in all_states:
             if state not in guessed_states:
                 missing_states.append(state)
-        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
